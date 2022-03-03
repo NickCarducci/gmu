0d31bec7a4cc7ef70779c3a9ef873f6f1ec961f3
Nick Carducci <nmcarducci@gmail.com>
2022-03-03 16:56:04 -0500

    Create edu.js

---
+++ src/edu.js
@@ -0,0 +1,692 @@
+import React from "react";
+const edu = {
+  "United States": {
+    "2014": "11,003",
+    "2015": "11,392",
+    "2016": "11,763",
+    "2017": "12,201",
+    "2018": "12,559",
+    "2019": "13,187"
+  },
+  Alabama: {
+    "2014": "9,028",
+    "2015": "9,128",
+    "2016": "9,243",
+    "2017": "9,511",
+    "2018": "9,696",
+    "2019": "10,077"
+  },
+  Alaska: {
+    "2014": "18,416",
+    "2015": "20,172",
+    "2016": "17,510",
+    "2017": "17,838",
+    "2018": "17,726",
+    "2019": "18,394"
+  },
+  Arizona: {
+    "2014": "7,528",
+    "2015": "7,489",
+    "2016": "7,613",
+    "2017": "8,004",
+    "2018": "8,255",
+    "2019": "8,625"
+  },
+  Arkansas: {
+    "2014": "9,616",
+    "2015": "9,694",
+    "2016": "9,846",
+    "2017": "9,967",
+    "2018": "10,139",
+    "2019": "10,388"
+  },
+  California: {
+    "2014": "9,595",
+    "2015": "10,467",
+    "2016": "11,495",
+    "2017": "12,145",
+    "2018": "12,143",
+    "2019": "14,035"
+  },
+  Colorado: {
+    "2014": "8,985",
+    "2015": "9,245",
+    "2016": "9,575",
+    "2017": "9,809",
+    "2018": "10,202",
+    "2019": "11,030"
+  },
+  Connecticut: {
+    "2014": "17,745",
+    "2015": "18,377",
+    "2016": "18,958",
+    "2017": "19,322",
+    "2018": "20,635",
+    "2019": "21,310"
+  },
+  Delaware: {
+    "2014": "13,938",
+    "2015": "14,120",
+    "2016": "14,713",
+    "2017": "15,302",
+    "2018": "15,639",
+    "2019": "16,315"
+  },
+  "District of Columbia": {
+    "2014": "18,485",
+    "2015": "19,396",
+    "2016": "20,235",
+    "2017": "21,974",
+    "2018": "22,759",
+    "2019": "22,406"
+  },
+  Florida: {
+    "2014": "8,755",
+    "2015": "8,881",
+    "2016": "8,920",
+    "2017": "9,075",
+    "2018": "9,346",
+    "2019": "9,645"
+  },
+  Georgia: {
+    "2014": "9,202",
+    "2015": "9,427",
+    "2016": "9,769",
+    "2017": "10,205",
+    "2018": "10,810",
+    "2019": "11,228"
+  },
+  Hawaii: {
+    "2014": "12,400",
+    "2015": "12,855",
+    "2016": "13,748",
+    "2017": "14,322",
+    "2018": "15,242",
+    "2019": "16,132"
+  },
+  Idaho: {
+    "2014": "6,621",
+    "2015": "6,923",
+    "2016": "7,157",
+    "2017": "7,486",
+    "2018": "7,771",
+    "2019": "7,985"
+  },
+  Illinois: {
+    "2014": "13,077",
+    "2015": "13,755",
+    "2016": "14,180",
+    "2017": "15,337",
+    "2018": "15,757",
+    "2019": "16,227"
+  },
+  Indiana: {
+    "2014": "9,548",
+    "2015": "9,687",
+    "2016": "9,856",
+    "2017": "10,045",
+    "2018": "10,262",
+    "2019": "10,397"
+  },
+  Iowa: {
+    "2014": "10,668",
+    "2015": "10,944",
+    "2016": "11,150",
+    "2017": "11,461",
+    "2018": "11,732",
+    "2019": "11,907"
+  },
+  Kansas: {
+    "2014": "9,940",
+    "2015": "10,040",
+    "2016": "9,960",
+    "2017": "10,961",
+    "2018": "11,653",
+    "2019": "11,926"
+  },
+  Kentucky: {
+    "2014": "9,314",
+    "2015": "9,630",
+    "2016": "9,863",
+    "2017": "10,121",
+    "2018": "11,110",
+    "2019": "11,291"
+  },
+  Louisiana: {
+    "2014": "10,749",
+    "2015": "11,010",
+    "2016": "11,038",
+    "2017": "11,199",
+    "2018": "11,452",
+    "2019": "11,755"
+  },
+  Maine: {
+    "2014": "12,707",
+    "2015": "13,257",
+    "2016": "13,282",
+    "2017": "13,690",
+    "2018": "14,152",
+    "2019": "14,614"
+  },
+  Maryland: {
+    "2014": "14,003",
+    "2015": "14,192",
+    "2016": "14,206",
+    "2017": "14,848",
+    "2018": "14,762",
+    "2019": "15,148"
+  },
+  Massachusetts: {
+    "2014": "15,087",
+    "2015": "15,592",
+    "2016": "15,593",
+    "2017": "16,193",
+    "2018": "17,058",
+    "2019": "17,748"
+  },
+  Michigan: {
+    "2014": "11,110",
+    "2015": "11,482",
+    "2016": "11,668",
+    "2017": "11,907",
+    "2018": "12,345",
+    "2019": "12,756"
+  },
+  Minnesota: {
+    "2014": "11,464",
+    "2015": "11,949",
+    "2016": "12,382",
+    "2017": "12,647",
+    "2018": "12,995",
+    "2019": "13,387"
+  },
+  Mississippi: {
+    "2014": "8,263",
+    "2015": "8,456",
+    "2016": "8,702",
+    "2017": "8,771",
+    "2018": "8,935",
+    "2019": "9,284"
+  },
+  Missouri: {
+    "2014": "9,875",
+    "2015": "10,147",
+    "2016": "10,313",
+    "2017": "10,589",
+    "2018": "10,906",
+    "2019": "11,200"
+  },
+  Montana: {
+    "2014": "11,017",
+    "2015": "11,033",
+    "2016": "11,348",
+    "2017": "11,457",
+    "2018": "11,680",
+    "2019": "11,988"
+  },
+  Nebraska: {
+    "2014": "11,726",
+    "2015": "11,946",
+    "2016": "12,299",
+    "2017": "12,579",
+    "2018": "12,816",
+    "2019": "12,743"
+  },
+  Nevada: {
+    "2014": "8,414",
+    "2015": "8,615",
+    "2016": "8,960",
+    "2017": "9,320",
+    "2018": "9,295",
+    "2019": "9,344"
+  },
+  "New Hampshire": {
+    "2014": "14,332",
+    "2015": "14,697",
+    "2016": "15,340",
+    "2017": "15,683",
+    "2018": "16,893",
+    "2019": "17,462"
+  },
+  "New Jersey": {
+    "2014": "17,899",
+    "2015": "18,235",
+    "2016": "18,402",
+    "2017": "18,920",
+    "2018": "19,526",
+    "2019": "20,512"
+  },
+  "New Mexico": {
+    "2014": "9,008",
+    "2015": "9,752",
+    "2016": "9,693",
+    "2017": "9,881",
+    "2018": "9,582",
+    "2019": "10,177"
+  },
+  "New York": {
+    "2014": "20,607",
+    "2015": "21,206",
+    "2016": "22,366",
+    "2017": "23,091",
+    "2018": "24,048",
+    "2019": "25,139"
+  },
+  "North Carolina": {
+    "2014": "8,512",
+    "2015": "8,687",
+    "2016": "8,792",
+    "2017": "9,072",
+    "2018": "9,377",
+    "2019": "9,857"
+  },
+  "North Dakota": {
+    "2014": "12,358",
+    "2015": "13,320",
+    "2016": "13,373",
+    "2017": "13,760",
+    "2018": "13,758",
+    "2019": "14,004"
+  },
+  Ohio: {
+    "2014": "11,349",
+    "2015": "11,637",
+    "2016": "12,102",
+    "2017": "12,645",
+    "2018": "13,027",
+    "2019": "13,538"
+  },
+  Oklahoma: {
+    "2014": "7,829",
+    "2015": "8,082",
+    "2016": "8,097",
+    "2017": "7,959",
+    "2018": "8,239",
+    "2019": "9,323"
+  },
+  Oregon: {
+    "2014": "9,945",
+    "2015": "10,442",
+    "2016": "10,842",
+    "2017": "11,264",
+    "2018": "11,920",
+    "2019": "12,450"
+  },
+  Pennsylvania: {
+    "2014": "13,961",
+    "2015": "14,717",
+    "2016": "15,437",
+    "2017": "15,798",
+    "2018": "16,395",
+    "2019": "16,864"
+  },
+  "Rhode Island": {
+    "2014": "14,767",
+    "2015": "15,179",
+    "2016": "15,532",
+    "2017": "15,962",
+    "2018": "16,142",
+    "2019": "16,750"
+  },
+  "South Carolina": {
+    "2014": "9,732",
+    "2015": "9,953",
+    "2016": "10,249",
+    "2017": "10,590",
+    "2018": "10,856",
+    "2019": "11,140"
+  },
+  "South Dakota": {
+    "2014": "8,881",
+    "2015": "8,937",
+    "2016": "9,176",
+    "2017": "9,939",
+    "2018": "10,073",
+    "2019": "10,139"
+  },
+  Tennessee: {
+    "2014": "8,630",
+    "2015": "8,723",
+    "2016": "8,810",
+    "2017": "9,184",
+    "2018": "9,544",
+    "2019": "9,868"
+  },
+  Texas: {
+    "2014": "8,592",
+    "2015": "8,861",
+    "2016": "9,016",
+    "2017": "9,375",
+    "2018": "9,606",
+    "2019": "9,827"
+  },
+  Utah: {
+    "2014": "6,500",
+    "2015": "6,575",
+    "2016": "6,953",
+    "2017": "7,179",
+    "2018": "7,628",
+    "2019": "8,014"
+  },
+  Vermont: {
+    "2014": "16,988",
+    "2015": "18,039",
+    "2016": "17,873",
+    "2017": "18,290",
+    "2018": "19,341",
+    "2019": "20,315"
+  },
+  Virginia: {
+    "2014": "10,973",
+    "2015": "11,237",
+    "2016": "11,432",
+    "2017": "11,886",
+    "2018": "12,216",
+    "2019": "12,641"
+  },
+  Washington: {
+    "2014": "10,202",
+    "2015": "10,735",
+    "2016": "11,534",
+    "2017": "11,989",
+    "2018": "12,995",
+    "2019": "14,223"
+  },
+  "West Virginia": {
+    "2014": "11,251",
+    "2015": "11,359",
+    "2016": "11,291",
+    "2017": "11,554",
+    "2018": "11,334",
+    "2019": "12,010"
+  },
+  Wisconsin: {
+    "2014": "11,186",
+    "2015": "11,375",
+    "2016": "11,456",
+    "2017": "11,968",
+    "2018": "12,285",
+    "2019": "12,598"
+  },
+  Wyoming: {
+    "2014": "15,797",
+    "2015": "16,055",
+    "2016": "16,442",
+    "2017": "16,537",
+    "2018": "16,224",
+    "2019": "16,304"
+  }
+};
+
+class EDU extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //let testing = [];
+    let noData = [];
+    let date = [];
+    let currentData = [];
+    let currentDataData = [];
+    let states = [];
+    var all = null;
+    Object.keys(edu).forEach((state, i) => {
+      states.push(state);
+      const dta = Object.values(edu)[i];
+      Object.keys(dta).forEach((yr, i) => {
+        if ("United States" === state) {
+          const num = Number(Object.values(dta)[i].replace(",", ""));
+          all = num;
+          const year = Number(yr);
+          date.push(year);
+          noData.push([year, 0]);
+          currentData.push(num);
+          currentDataData.push([year, num]);
+        }
+      });
+    });
+    var lowPPCS = Math.min(...currentData);
+    var lowDate = Math.min(...date);
+    var highPPCS = Math.max(...currentData, all);
+    var highDate = Math.max(...date);
+    var state = {
+      all,
+      states,
+      highPPCS,
+      currentDataData,
+      noData,
+      yAxisPPCS: highPPCS - 0,
+      xAxisPPCS: highDate - lowDate,
+      lowPPCS,
+      highDate,
+      lowDate
+    };
+    this.state = state;
+  }
+  componentDidUpdate = () => {
+    if (this.state.state !== this.state.laststate) {
+      this.setState({ laststate: this.state.state }, () => {
+        let noData = [];
+        let date = [];
+        let currentData = [];
+        let currentDataData = [];
+        let states = [];
+        Object.keys(edu).forEach((state, i) => {
+          states.push(state);
+          const dta = Object.values(edu)[i];
+          Object.keys(dta).forEach((yr, i) => {
+            if (this.state.state === state) {
+              const num = Number(Object.values(dta)[i].replace(",", ""));
+              const year = Number(yr);
+              date.push(year);
+              noData.push([year, 0]);
+              currentData.push(num);
+              currentDataData.push([year, num]);
+            }
+          });
+        });
+        var lowPPCS = Math.min(...currentData);
+        var lowDate = Math.min(...date);
+        var highPPCS = Math.max(...currentData, this.state.all);
+        var highDate = Math.max(...date);
+        var state = {
+          states,
+          highPPCS,
+          currentDataData,
+          noData,
+          yAxisPPCS: highPPCS - 0,
+          xAxisPPCS: highDate - lowDate,
+          lowPPCS,
+          highDate,
+          lowDate
+        };
+        this.setState(state);
+      });
+    }
+  };
+  render() {
+    const { states } = this.state;
+    const labelstyle = {
+      backgroundColor: "rgba(50,120,200,.6)",
+      top: "0px",
+      height: "min-content",
+      display: "flex",
+      maxWidth: "100%",
+      left: "2px",
+      flexWrap: "wrap"
+    };
+    const buttonStyle = {
+      userSelect: "none",
+      border: "1px solid black",
+      color: "black",
+      backgroundColor: "rgb(220,220,220)",
+      borderRadius: "4px",
+      padding: "5px",
+      margin: "2px"
+    };
+
+    const linecss = {
+      left: "0px",
+      bottom: "0px",
+      display: "flex",
+      position: "absolute",
+      width: "100%",
+      height: "200px",
+      transform: "translate(0%,0%) scale(1,-1)"
+    };
+    const shortNumber = (scler, notRound) => {
+      var newnum = String(Math.round(scler));
+      if (notRound) newnum = String(scler);
+      var app = null;
+      var decimal = null;
+      const suff = ["", "k", "m", "b", "t"];
+      for (let i = 0; i < suff.length; i++) {
+        if (newnum.length > 3) {
+          decimal = newnum[newnum.length - 3];
+          newnum = newnum.substring(0, newnum.length - 3);
+        } else {
+          app = i;
+          break;
+        }
+      }
+      return newnum + (decimal ? "." + decimal : "") + suff[app];
+    };
+    //console.log(this.state.oilprice);
+    const noData = this.state.noData.map(([x, y]) => [
+      ((x - this.state.lowDate) / this.state.xAxis) *
+        0.9 *
+        this.props.lastWidth,
+      0
+    ]);
+    //console.log(this.state.oilprice);
+
+    const currentDataData = this.state.currentDataData.map(([x, y]) => [
+      ((x - this.state.lowDate) / this.state.xAxisPPCS) *
+        0.9 *
+        this.props.lastWidth,
+      ((y - 0) / this.state.yAxisPPCS) * 150
+    ]);
+    return (
+      <div
+        style={{
+          width: "100%",
+          minHeight: "240px",
+          position: "relative",
+          backgroundColor: "rgb(0,40,0)"
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            top: "0px",
+            display: "flex",
+            position: "absolute",
+            left: "0px",
+            flexDirection: "column",
+            zIndex: "1"
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "rgba(255,255,255,.3)",
+              padding: "4px 8px",
+              position: "absolute",
+              right: "0px"
+            }}
+          >
+            <select
+              state={this.state.state}
+              onChange={(name) => {
+                this.setState({ state: name.target.value });
+              }}
+            >
+              {states.map((name) => (
+                <option key={name}>{name}</option>
+              ))}
+            </select>
+            EDU: ${shortNumber(this.state.lowPPCS)}-
+            {shortNumber(this.state.highPPCS)} product
+          </div>
+          <div
+            style={{
+              width: "min-content",
+              padding: "4px 8px",
+              backgroundColor: "rgba(250,250,250,.6)",
+              height: "min-content",
+              display: "flex",
+              position: "relative"
+            }}
+          >
+            {this.state.lowDate}
+            &nbsp;-&nbsp;
+            {this.state.highDate}
+          </div>
+        </div>
+        <div style={{ transform: "translate(0px,230px)" }}>
+          <svg
+            className="all"
+            style={linecss}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {noData.map(
+              ([x, y], i) =>
+                !isNaN(x) &&
+                !isNaN(y) && (
+                  <rect
+                    x={x}
+                    y={y}
+                    width={4}
+                    height={4}
+                    stroke="rgb(230,230,230)"
+                    fill="transparent"
+                    strokeWidth={0}
+                    key={i}
+                  />
+                )
+            )}
+            {currentDataData.map(
+              ([x, y], i) =>
+                !isNaN(x) &&
+                !isNaN(y) && (
+                  <rect
+                    x={x}
+                    y={y}
+                    width={2}
+                    height={2}
+                    stroke="white"
+                    fill="blue"
+                    strokeWidth={1}
+                    key={i}
+                  />
+                )
+            )}
+          </svg>
+        </div>
+        {/*<div
+          style={{
+            backgroundColor: "rgba(250,250,250,0.6)",
+            top: "10px",
+            height: "40px",
+            display: "flex",
+            position: "relative",
+            width: "100%",
+            left: "2px",
+            zIndex: "0",
+            overflowX: "auto",
+            overflowY: "hidden"
+          }}
+        >
+          <div
+            style={{
+              fontSize: "15px",
+              display: "flex",
+              position: "absolute",
+              width: "max-content"
+            }}
+          ></div>
+          </div>*/}
+      </div>
+    );
+  }
+}
+
+export default EDU;
